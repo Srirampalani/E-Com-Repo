a9f40e155b0d1bfe1aa72e345f6803bfaf0edc42
Srirampalani <sriramnadhiyar@gmail.com>
2022-11-02 06:22:04 -0700

    new commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { importProvidersFrom, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { CartComponent } from './component/cart/cart.component';
+import { ProductComponent } from './component/product/product.component';
+import { HttpClientModule } from "@angular/common/http";
+import { FooterComponent } from './component/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    CartComponent,
+    ProductComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
